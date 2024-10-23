4f2a0469063bd0f975a796858e52cb7db0e85a91
JapiKredi <jasper.bongers@yahoo.com>
2024-10-23 20:06:10 +0800

    first commit

---
+++ calculator.py
@@ -0,0 +1,11 @@
+#x = int(input("What is x? "))
+#y = int(input("What is y? "))
+#z = y + x
+#print(z)
+
+x = float(input("What is x? "))
+y = float(input("What is y? "))
+z = round(y + x)
+
+print(f"{z:,}")
+
